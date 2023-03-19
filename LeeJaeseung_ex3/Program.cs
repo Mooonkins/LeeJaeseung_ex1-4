950f6fc5099c783f3662f24b539f43898e27f6ff
jaeseung Lee <quasarjae72@gmail.com>
2023-03-19 23:24:20 +0900

    초기 커밋.

---
+++ LeeJaeseung_ex3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeeJaeseung_ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int c = 1;
+            int d = 0;
+            while (c <= 5)
+            {
+                d = 0;
+                do
+                {
+                    Console.Write("*");
+                    d++;
+                } while (d < c);
+                Console.WriteLine();
+                c++;
+            }
+            Console.WriteLine();
+
+            int r = 5;
+            int t = 0;
+
+            while (r > 0)
+            {
+                t = r;
+                do
+                {
+                    Console.Write("*");
+                    t--;
+                } while (t > 0);                
+                Console.WriteLine();
+                r--;
+            }
+        }
+    }
+}
---
+++ LeeJaeseung_ex4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeeJaeseung_ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("반복 횟수를 입력하세요 : ");
+            string star_Input = Console.ReadLine();
+            int star_Number = int.Parse(star_Input);
+
+            if (star_Number > 0)
+            {
+                for (int i = 0; i < star_Number; i++)
+                {
+                    for (int j = 0; j <= i; j++)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            else if (star_Number <= 0)
+                Console.WriteLine("0보다 작거나 같은 수는 입력할 수 없습니다");
+        }
+    }
+}
